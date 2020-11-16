24c36df11f01e836448b10624eb70c2e028a2d45
georges sysaath <georges.sysaath@gmail.com>
2020-11-16 15:17:20 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: "pages#dashboard"
+  resources :vinyles, only: [ :index, :show, :new, :create, :destroy] do
+    resources :trades, only: [ :create ]
+  end
+  resources :trades, only: [ :destroy ]
 end
