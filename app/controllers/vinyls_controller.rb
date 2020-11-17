575f62690dc4ad0f958f77b3c4f7d6f4b8ae34f5
Olivier Calmels <calmels.o@gmail.com>
2020-11-17 12:20:05 +0100

    coded 7 CRUD actions for Vinyls controller.

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,9 +1,51 @@
 class VinylsController < ApplicationController
+  before_action :set_vinyl, only: %i[show edit update destroy]
+
   def index
     @vinyls = Vinyl.all
   end
 
   def show
-    # @vinyl = Vinyl.find(params[:id])
+  end
+
+  def new
+    @vinyl = Vinyl.new
+  end
+
+  def create
+    @vinyl = Vinyl.new
+    if @vinyl.save
+      redirect_to vinyl_path(@vinyl)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @vinyl.update(vinyl_params)
+      redirect_to vinyl_path(@vinyl)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @vinyl.destroy
+    redirect_to vinyls_path
+  end
+
+  private
+
+  def set_vinyl
+    @vinyl = Vinyl.find(params[:id])
+  end
+
+  def vinyl_params
+    params.require(:vinyl).permit(:name, :year, :user_id, :artist, :genre,
+                                  :label, :quality, :dimension, :price_per_day,
+                                  :available)
   end
 end
