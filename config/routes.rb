dfe0b3f56f8ebd4400815d6b13aa964138e8eb2f
georges sysaath <georges.sysaath@gmail.com>
2020-11-17 17:45:12 +0100

    log out log in and dashboard fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/dashboard', to: "pages#dashboard"
