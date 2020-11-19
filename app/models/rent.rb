9f314e50acaeab82751ca935363ffacf43e6cbae
Olivier Calmels <calmels.o@gmail.com>
2020-11-19 12:35:36 +0100

    Search bar is active with PgSearch. Search Button routes at Vinyls index view.

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -5,15 +5,6 @@ class Rent < ApplicationRecord
   validates :start_date, presence: true, if: :start_is_possible?
   validates :end_date, presence: true, if: :rental_is_possible?
 
-  include PgSearch::Model
-  pg_search_scope :global_search,
-    associated_against: {
-      vinyl: [ :title, :artist ]
-    },
-    using: {
-      tsearch: { prefix: true }
-    }
-
   def rental_is_possible?
     if start_date >= end_date
       errors.add(:end_date, ' must exist after the rental start day')
