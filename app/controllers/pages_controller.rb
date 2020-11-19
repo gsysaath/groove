62003fb3925d768be99a785d699b33449a1948ba
amalgouia <amal_gouia@hotmail.fr>
2020-11-19 12:16:36 +0100

    map ok dans la show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,13 +9,6 @@ class PagesController < ApplicationController
   end
 
   def random
-    @users = User.all
-    @markers = @users.geocoded.map do |user| {
-      lat: user.latitude,
-      lng: user.longitude,
-      # infoWindow: render_to_string(partial: "info_window", locals: { flat: flat })
-      # image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
-    }
-    end
   end
+
 end
