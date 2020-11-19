675db3cbb9b5b051331af248676baea6e49ad5d8
Olivier Calmels <calmels.o@gmail.com>
2020-11-19 10:56:46 +0100

    gem pg_search added

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'simple_form'
 gem 'devise'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'cloudinary', '~> 1.16.0'
+gem 'pg_search', '~> 2.3.0'
 
 
 
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -5,6 +5,15 @@ class Rent < ApplicationRecord
   validates :start_date, presence: true, if: :start_is_possible?
   validates :end_date, presence: true, if: :rental_is_possible?
 
+  include PgSearch::Model
+  pg_search_scope :global_search,
+    associated_against: {
+      vinyl: [ :title, :artist ]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
   def rental_is_possible?
     if start_date >= end_date
       errors.add(:end_date, ' must exist after the rental start day')
@@ -18,4 +27,5 @@ class Rent < ApplicationRecord
       false
     end
   end
+
 end
