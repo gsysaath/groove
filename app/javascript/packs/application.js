a4ce53ba64c528300790c5b602a2b8b2b6dfbc65
Alexandre Wotawa <awotawa@gmail.com>
2020-11-19 23:46:02 +0100

    fixed the bugs on the flatpicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require ("jquery_ujs")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
