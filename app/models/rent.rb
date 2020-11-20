0731bd5ddf1ac1324a270e6d1888f4dcce314145
Olivier Calmels <calmels.o@gmail.com>
2020-11-20 15:36:00 +0100

    Seeding tout azimuts pour la démo.

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -2,7 +2,7 @@ class Rent < ApplicationRecord
   belongs_to :vinyl
   belongs_to :user
 
-  validates :start_date, presence: true, if: :start_is_possible?
+  validates :start_date, presence: true #, if: :start_is_possible?
   validates :end_date, presence: true, if: :rental_is_possible?
 
   STATUS = ["pending approval", "rental to come", "currently renting", "previous rental", "canceled rental" ].freeze
@@ -15,10 +15,10 @@ class Rent < ApplicationRecord
     end
   end
 
-  def start_is_possible?
-    if start_date < Date.today
-      errors.add(:start_date, " must be equal or superior to today's date")
-      false
-    end
-  end
+  # def start_is_possible?
+  #   if start_date < Date.today
+  #     errors.add(:start_date, " must be equal or superior to today's date")
+  #     false
+  #   end
+  # end
 end
