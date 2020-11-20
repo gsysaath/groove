11cfdb5518df3adbcf699b0078e985d3364799cd
georges sysaath <georges.sysaath@gmail.com>
2020-11-20 14:50:27 +0100

    fixed the rent new

--- app/javascript/vanilla/price.js
+++ app/javascript/vanilla/price.js
@@ -1,11 +1,11 @@
 const price = () => {
-  const priceTotal = document.querySelector(".price-total");
-  const priceDay = document.querySelector(".price-per-day");
-  const rents = document.querySelectorAll(".datepicker");
   const card = document.querySelector(".card-book");
   console.log("here")
 
   if (card){
+    const priceTotal = document.querySelector(".price-total");
+    const priceDay = document.querySelector(".price-per-day");
+    const rents = document.querySelectorAll(".datepicker");
     card.addEventListener("change", (event) => {
       console.log("here")
       let date1 = new Date(rents[1].value);
