f275220ccfd1ecf1fa686af63b81576fc90d5dcd
georges sysaath <georges.sysaath@gmail.com>
2020-11-20 16:05:45 +0100

    done

--- app/javascript/vanilla/price.js
+++ app/javascript/vanilla/price.js
@@ -1,34 +1,22 @@
 const price = () => {
   const card = document.querySelector(".card-book");
-  console.log("here")
 
   if (card){
-    const priceTotal = document.querySelector(".price-total");
-    const priceDay = document.querySelector(".price-per-day");
-    const rents = document.querySelectorAll(".datepicker");
-    card.addEventListener("change", (event) => {
-      console.log("here")
-      let date1 = new Date(rents[1].value);
-      console.log(date1);
-      let date2 = new Date(rents[2].value);
-      let difDays = Math.round((date2 - date1) / 86400000);
-      console.log(difDays);
-      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
-      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
-      priceTotal.innerText = 0;
-    })
-    card.addEventListener("change", (event) => {
-      console.log("here")
-      let date1 = new Date(rents[1].value);
-      console.log(date1);
-      let date2 = new Date(rents[2].value);
-      let difDays = Math.round((date2 - date1) / 86400000);
-      console.log(difDays);
-      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
-      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
-      priceTotal.innerText = 0;
-    })
 
+    card.addEventListener("change", (event) => {
+      const priceTotal = document.querySelector(".price-total");
+      const priceDay = document.querySelector(".price-per-day");
+      const rents = document.querySelectorAll(".datepicker");
+      let date1 = new Date(rents[1].value);
+      console.log(rents[1].value);
+      console.log(date1);
+      let date2 = new Date(rents[2].value);
+      let difDays = Math.round((date2 - date1) / 86400000);
+      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
+      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
+      priceTotal.innerText = 0;
+      console.log (difDays);
+    })
   }
 
 
