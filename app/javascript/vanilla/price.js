a3b8608d6c37157debe88742ff9eb80fca809c8e
georges sysaath <georges.sysaath@gmail.com>
2020-11-20 13:28:30 +0100

    my music btn

--- app/javascript/vanilla/price.js
+++ app/javascript/vanilla/price.js
@@ -5,29 +5,32 @@ const price = () => {
   const card = document.querySelector(".card-book");
   console.log("here")
 
+  if (card){
+    card.addEventListener("change", (event) => {
+      console.log("here")
+      let date1 = new Date(rents[1].value);
+      console.log(date1);
+      let date2 = new Date(rents[2].value);
+      let difDays = Math.round((date2 - date1) / 86400000);
+      console.log(difDays);
+      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
+      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
+      priceTotal.innerText = 0;
+    })
+    card.addEventListener("change", (event) => {
+      console.log("here")
+      let date1 = new Date(rents[1].value);
+      console.log(date1);
+      let date2 = new Date(rents[2].value);
+      let difDays = Math.round((date2 - date1) / 86400000);
+      console.log(difDays);
+      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
+      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
+      priceTotal.innerText = 0;
+    })
 
-    card.addEventListener("change", (event) => {
-      console.log("here")
-      let date1 = new Date(rents[1].value);
-      console.log(date1);
-      let date2 = new Date(rents[2].value);
-      let difDays = Math.round((date2 - date1) / 86400000);
-      console.log(difDays);
-      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
-      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
-      priceTotal.innerText = 0;
-    })
-    card.addEventListener("change", (event) => {
-      console.log("here")
-      let date1 = new Date(rents[1].value);
-      console.log(date1);
-      let date2 = new Date(rents[2].value);
-      let difDays = Math.round((date2 - date1) / 86400000);
-      console.log(difDays);
-      parseFloat(priceDay.innerText) * (difDays + 1) >= 0 ?
-      priceTotal.innerText = (parseFloat(priceDay.innerText) * (difDays + 1)).toFixed(2) :
-      priceTotal.innerText = 0;
-    })
+  }
+
 
 }
 
